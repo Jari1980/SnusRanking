d4e113151d630362bbca31025a301e8d5e0baf5d
jari.tenhunen <jari.tenhunen@outlook.com>
2025-08-22 17:36:34 +0200

    Image to Snus added as bit[], added dummy images to seed

---
+++ SnusRanking/Migrations/20250822153236_third.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SnusRanking.Migrations
+{
+    /// <inheritdoc />
+    public partial class third : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "Pic",
+                table: "Snus",
+                type: "varbinary(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Pic",
+                table: "Snus");
+        }
+    }
+}
--- SnusRanking/Models/SeedSnusData.cs
+++ SnusRanking/Models/SeedSnusData.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Components.Sections;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations;
 using SnusRanking.Data;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace SnusRanking.Models
 {
@@ -18,55 +20,63 @@ namespace SnusRanking.Models
                 }
 
                 context.Snus.AddRange(
-                    new Snus
-                    {
-                        Name = "General Orginal",
-                        Type = "Portion",
-                        Flavor = "Rich and flavorfull tobaccocharacter with a hint of bergamott, dried weed, leather and tee"
-                    },
-                    new Snus
-                    {
-                        Name = "General Orginal White",
-                        Type = "Portion",
-                        Flavor = "Rich and flavorfull tobaccocharacter with a hint of bergamott, dried weed, leather and tee"
-                    },
-                    new Snus
-                    {
-                        Name = "Kronan",
-                        Type = "Portion",
-                        Flavor = "Rich tobaccocharacter with hint of viol, citrus and green herbs"
-                    },
-                    new Snus
-                    {
-                        Name = "Kronan White",
-                        Type = "Portion",
-                        Flavor = "Rich tobaccocharacter with hint of viol, citrus and green herbs"
-                    },
-                    new Snus
-                    {
-                        Name = "Kapten White",
-                        Type = "Portion",
-                        Flavor = "Tobacco and bergamott"
-                    },
-                    new Snus
-                    {
-                        Name = "Ettan",
-                        Type = "Portion",
-                        Flavor = "Tobacco, smoke, malt and a hint of dark chocolate"
-                    },
-                    new Snus
-                    {
-                        Name = "Grov",
-                        Type = "Portion",
-                        Flavor = "Rich tobacco with geranium, dried herbs, nuts and tar"
-                    },
-                    new Snus
-                    {
-                        Name = "Kaliber",
-                        Type = "Portion",
-                        Flavor = "Tobacco with hint of citrus, herbs and pomegranate"
-                    }
-                    );
+                new Snus
+                {
+                    Name = "General Orginal",
+                    Type = "Portion",
+                    Flavor = "Rich and flavorfull tobaccocharacter with a hint of bergamott, dried weed, leather and tee",
+                    Pic = File.ReadAllBytes("wwwroot/images/GeneralOrginal.png")
+                },
+                new Snus
+                {
+                    Name = "General Orginal White",
+                    Type = "Portion",
+                    Flavor = "Rich and flavorfull tobaccocharacter with a hint of bergamott, dried weed, leather and tee",
+                    Pic = File.ReadAllBytes("wwwroot/images/GeneralOrginalWhite.png")
+                },
+                new Snus
+                {
+                    Name = "Kronan",
+                    Type = "Portion",
+                    Flavor = "Rich tobaccocharacter with hint of viol, citrus and green herbs",
+                    Pic = File.ReadAllBytes("wwwroot/images/Kronan.png")
+                },
+                new Snus
+                {
+                    Name = "Kronan White",
+                    Type = "Portion",
+                    Flavor = "Rich tobaccocharacter with hint of viol, citrus and green herbs",
+                    Pic = File.ReadAllBytes("wwwroot/images/KronanWhite.png")
+                },
+                new Snus
+                {
+                    Name = "Kapten",
+                    Type = "Portion",
+                    Flavor = "Tobacco and bergamott",
+                    Pic = File.ReadAllBytes("wwwroot/images/Kapten.png")
+                },
+                new Snus
+                {
+                    Name = "Ettan",
+                    Type = "Portion",
+                    Flavor = "Tobacco, smoke, malt and a hint of dark chocolate",
+                    Pic = File.ReadAllBytes("wwwroot/images/Ettan.png")
+                },
+                new Snus
+                {
+                    Name = "Grov",
+                    Type = "Portion",
+                    Flavor = "Rich tobacco with geranium, dried herbs, nuts and tar",
+                    Pic = File.ReadAllBytes("wwwroot/images/Grov.png")
+                },
+                new Snus
+                {
+                    Name = "Kaliber",
+                    Type = "Portion",
+                    Flavor = "Tobacco with hint of citrus, herbs and pomegranate",
+                    Pic = File.ReadAllBytes("wwwroot/images/Kaliber.png")
+                }
+                 );
 
                 context.SaveChanges();
             }
